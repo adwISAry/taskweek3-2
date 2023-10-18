8b58bdbfc25261f6eab6cbc42e12540288a11e23
adwISAry <mi75axs6d@code.edu.az>
2023-10-18 05:59:19 +0400

    Add project files.

---
+++ taskweek3,2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace taskweek3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFullName("Jonathan", "Will", " ");                
+        }
+
+        static void PrintFullName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName[0] + ". " + lastName);
+        }
+
+        static void PrintFullName(string firstName, string lastName, string middleName)
+        {
+            Console.WriteLine(firstName[0] + ". " + lastName + ". " + middleName[0] + ".");
+        }
+    }
+}
